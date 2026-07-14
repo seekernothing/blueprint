0a1271b068992af67d07bfadab81a486a0afd301
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2026-07-14 06:12:14 +0530

    initial commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <h2 className="text-xl text-amber-200 font-extrabold">
+    Hello World
+  </h2>
+}
